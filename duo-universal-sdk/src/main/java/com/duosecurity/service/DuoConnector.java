96f0d6ccbb8034ea2b3f299fc59cb5050ec41ee5
Benjamin Mark <benjamin.mark@ag.ch>
2021-10-05 09:31:56 +0200

    added proxy ability Redirect URIs dynamic for every call

--- duo-universal-sdk/src/main/java/com/duosecurity/service/DuoConnector.java
+++ duo-universal-sdk/src/main/java/com/duosecurity/service/DuoConnector.java
@@ -2,10 +2,14 @@ package com.duosecurity.service;
 
 import static com.duosecurity.Utils.getAndValidateUrl;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import com.duosecurity.exception.DuoException;
 import com.duosecurity.model.HealthCheckResponse;
 import com.duosecurity.model.TokenResponse;
-import java.io.IOException;
+
 import okhttp3.CertificatePinner;
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
@@ -15,87 +19,102 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class DuoConnector {
 
-  protected Retrofit retrofit;
+	protected Retrofit retrofit;
 
-  private static final int SUCCESS_STATUS_CODE = 200;
+	private static final int SUCCESS_STATUS_CODE = 200;
 
-  /**
-   * DuoConnector Constructor.
-   *
-   * @param apiHost         This value is the api host provided by Duo in the admin panel.
-   * @param caCerts         CA Certificates used to connect to Duo
-   *
-   * @throws DuoException   For issues getting and validating the URL
-   */
-  public DuoConnector(String apiHost, String[] caCerts) throws DuoException {
-    CertificatePinner duoCertificatePinner = new CertificatePinner.Builder()
-        .add(apiHost, caCerts)
-        .build();
+	/**
+	 * DuoConnector Constructor.
+	 *
+	 * @param apiHost This value is the api host provided by Duo in the admin panel.
+	 * @param caCerts CA Certificates used to connect to Duo
+	 *
+	 * @throws DuoException For issues getting and validating the URL
+	 */
+	public DuoConnector(String apiHost, String[] caCerts) throws DuoException {
+		this(apiHost, null, null, caCerts);
+	}
 
-    OkHttpClient client = new OkHttpClient.Builder().certificatePinner(
-                            duoCertificatePinner).build();
+	/**
+	 * DuoConnector Constructor.
+	 *
+	 * @param apiHost This value is the api host provided by Duo in the admin panel.
+	 * @param caCerts CA Certificates used to connect to Duo
+	 *
+	 * @throws DuoException For issues getting and validating the URL
+	 */
+	public DuoConnector(String apiHost, String proxyHost, Integer proxyPort, String[] caCerts) throws DuoException {
+		CertificatePinner duoCertificatePinner = new CertificatePinner.Builder().add(apiHost, caCerts).build();
 
-    retrofit = new Retrofit.Builder()
-            .baseUrl(getAndValidateUrl(apiHost, "").toString())
-            .addConverterFactory(JacksonConverterFactory.create())
-            .client(client)
-            .build();
-  }
+		OkHttpClient client = null;
 
-  /**
-   * Send Health Check request.
-   *
-   * @param clientId                The client id provided by Duo in the admin panel
-   * @param clientAssertion         A JWT the Duo Health Check endpoint needs
-   *
-   * @return HealthCheckResponse    Returns result from the Health Check
-   *
-   * @throws DuoException           For issues sending or receiving the request
-   */
-  public HealthCheckResponse duoHealthcheck(String clientId, String clientAssertion)
-      throws DuoException {
-    DuoService service = retrofit.create(DuoService.class);
-    Call<HealthCheckResponse> callSync = service.duoHealthCheck(clientId, clientAssertion);
-    try {
-      Response<HealthCheckResponse> response = callSync.execute();
-      return response.body();
-    } catch (IOException e) {
-      throw new DuoException(e.getMessage(), e);
-    }
-  }
+		if (proxyHost != null && proxyPort != null) {
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
 
-  /**
-   * Send request to exchange duoCode for an encoded JWT.
-   *
-   * @param userAgent           A user agent string
-   * @param grantType           A string that tells what type of exchange that will occur
-   * @param duoCode             An authentication session transaction id
-   * @param redirectUri         The URL to redirect back to after a successful auth
-   * @param clientAssertionType The type of client assertion used
-   * @param clientAssertion     JWT that holds information to verify that the owner of the duoCode
-   *                            is authorized to have it
-   *
-   * @return TokenResponse  Returns resulting response containing the JWT
-   *
-   * @throws DuoException   For issues sending or receiving the request,
-                            or failing to exchange a token
-   */
-  public TokenResponse exchangeAuthorizationCodeFor2FAResult(String userAgent, String grantType,
-                                                             String duoCode, String redirectUri,
-                                                             String clientAssertionType,
-                                                             String clientAssertion)
-      throws DuoException {
-    DuoService service = retrofit.create(DuoService.class);
-    Call<TokenResponse> callSync = service.exchangeAuthorizationCodeFor2FAResult(userAgent,
-                            grantType, duoCode, redirectUri, clientAssertionType, clientAssertion);
-    try {
-      Response<TokenResponse> response = callSync.execute();
-      if (response.code() != SUCCESS_STATUS_CODE || response.body() == null) {
-        throw new DuoException(response.message());
-      }
-      return response.body();
-    } catch (IOException e) {
-      throw new DuoException(e.getMessage(), e);
-    }
-  }
+			client = new OkHttpClient.Builder().certificatePinner(duoCertificatePinner).proxy(proxy).build();
+		} else {
+
+			client = new OkHttpClient.Builder().certificatePinner(duoCertificatePinner).build();
+
+		}
+
+		retrofit = new Retrofit.Builder().baseUrl(getAndValidateUrl(apiHost, "").toString())
+				.addConverterFactory(JacksonConverterFactory.create()).client(client).build();
+	}
+
+	/**
+	 * Send Health Check request.
+	 *
+	 * @param clientId        The client id provided by Duo in the admin panel
+	 * @param clientAssertion A JWT the Duo Health Check endpoint needs
+	 *
+	 * @return HealthCheckResponse Returns result from the Health Check
+	 *
+	 * @throws DuoException For issues sending or receiving the request
+	 */
+	public HealthCheckResponse duoHealthcheck(String clientId, String clientAssertion) throws DuoException {
+		DuoService service = retrofit.create(DuoService.class);
+		Call<HealthCheckResponse> callSync = service.duoHealthCheck(clientId, clientAssertion);
+		try {
+			Response<HealthCheckResponse> response = callSync.execute();
+			return response.body();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new DuoException(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * Send request to exchange duoCode for an encoded JWT.
+	 *
+	 * @param userAgent           A user agent string
+	 * @param grantType           A string that tells what type of exchange that
+	 *                            will occur
+	 * @param duoCode             An authentication session transaction id
+	 * @param redirectUri         The URL to redirect back to after a successful
+	 *                            auth
+	 * @param clientAssertionType The type of client assertion used
+	 * @param clientAssertion     JWT that holds information to verify that the
+	 *                            owner of the duoCode is authorized to have it
+	 *
+	 * @return TokenResponse Returns resulting response containing the JWT
+	 *
+	 * @throws DuoException For issues sending or receiving the request, or failing
+	 *                      to exchange a token
+	 */
+	public TokenResponse exchangeAuthorizationCodeFor2FAResult(String userAgent, String grantType, String duoCode,
+			String redirectUri, String clientAssertionType, String clientAssertion) throws DuoException {
+		DuoService service = retrofit.create(DuoService.class);
+		Call<TokenResponse> callSync = service.exchangeAuthorizationCodeFor2FAResult(userAgent, grantType, duoCode,
+				redirectUri, clientAssertionType, clientAssertion);
+		try {
+			Response<TokenResponse> response = callSync.execute();
+			if (response.code() != SUCCESS_STATUS_CODE || response.body() == null) {
+				throw new DuoException(response.message());
+			}
+			return response.body();
+		} catch (IOException e) {
+			throw new DuoException(e.getMessage(), e);
+		}
+	}
 }
--- duo-universal-sdk/src/test/java/com/duosecurity/ClientTest.java
+++ duo-universal-sdk/src/test/java/com/duosecurity/ClientTest.java
@@ -25,229 +25,231 @@ import static org.mockito.Mockito.verify;
 
 class ClientTest {
 
-    private static final String CLIENT_ID = "DIEN9ZH50WBGER236YT5";
-    private static final String CLIENT_SECRET = "IZjstQj23454IB2H1qhoQj23Ws2ddZfIOGSxOGSx";
-    private static final String API_HOST = "api-host.com";
-    private static final String HTTPS_REDIRECT_URI = "https://redirect-uri.com";
-    private static final String STATE = "abcdefghijklmnopqrstuvwxyz123456";
-    private static final String USERNAME = "username";
+	private static final String CLIENT_ID = "DIEN9ZH50WBGER236YT5";
+	private static final String CLIENT_SECRET = "IZjstQj23454IB2H1qhoQj23Ws2ddZfIOGSxOGSx";
+	private static final String API_HOST = "api-host.com";
+	private static final String HTTPS_REDIRECT_URI = "https://redirect-uri.com";
+	private static final String STATE = "abcdefghijklmnopqrstuvwxyz123456";
+	private static final String USERNAME = "username";
 
-    private Client client;
+	private Client client;
 
-    @BeforeEach
-    void setUp() throws DuoException {
-        this.client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
-        client.duoConnector = Mockito.mock(DuoConnector.class);
-    }
+	@BeforeEach
+	void setUp() throws DuoException {
+		this.client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, null, null, API_HOST).build();
+		client.duoConnector = Mockito.mock(DuoConnector.class);
+	}
 
-    @Test
-    void healthCheck_success() throws DuoException {
-        HealthCheckResponse healthCheckResponse = new HealthCheckResponse();
-        healthCheckResponse.setStat("OK");
-        Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenReturn(healthCheckResponse);
+	@Test
+	void healthCheck_success() throws DuoException {
+		HealthCheckResponse healthCheckResponse = new HealthCheckResponse();
+		healthCheckResponse.setStat("OK");
+		Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenReturn(healthCheckResponse);
 
-        HealthCheckResponse result = client.healthCheck();
+		HealthCheckResponse result = client.healthCheck();
 
-        assertEquals(healthCheckResponse, result);
-    }
+		assertEquals(healthCheckResponse, result);
+	}
 
-    @Test
-    void healthCheck_throws_exception() throws DuoException {
-        Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenThrow(new DuoException("error"));
-        HealthCheckResponse result = null;
-        try {
-            result = client.healthCheck();
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertEquals(e.getMessage(), "error");
-        }
-    }
+	@Test
+	void healthCheck_throws_exception() throws DuoException {
+		Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenThrow(new DuoException("error"));
+		HealthCheckResponse result = null;
+		try {
+			result = client.healthCheck();
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertEquals(e.getMessage(), "error");
+		}
+	}
 
-    @Test
-    void healthCheck_throws_exception_when_stat_not_OK() throws DuoException {
-        HealthCheckResponse healthCheckResponse = new HealthCheckResponse();
-        healthCheckResponse.setStat("FAIL");
-        healthCheckResponse.setMessage("invalid client secret");
-        Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenReturn(healthCheckResponse);
-        HealthCheckResponse result = null;
-        try {
-            result = client.healthCheck();
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertEquals(e.getMessage(), "invalid client secret");
-        }
-    }
+	@Test
+	void healthCheck_throws_exception_when_stat_not_OK() throws DuoException {
+		HealthCheckResponse healthCheckResponse = new HealthCheckResponse();
+		healthCheckResponse.setStat("FAIL");
+		healthCheckResponse.setMessage("invalid client secret");
+		Mockito.when(client.duoConnector.duoHealthcheck(anyString(), any())).thenReturn(healthCheckResponse);
+		HealthCheckResponse result = null;
+		try {
+			result = client.healthCheck();
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertEquals(e.getMessage(), "invalid client secret");
+		}
+	}
 
-    @Test
-    void createAuthUrl_success() throws DuoException {
-        String urlString = client.createAuthUrl(USERNAME, STATE);
-        try {
-            URL authUrl = new URL(urlString);
-            assertEquals(authUrl.getHost(), API_HOST);
-            assertTrue(authUrl.getQuery().contains("redirect_uri=" + HTTPS_REDIRECT_URI));
-            assertTrue(authUrl.getQuery().contains("client_id=" + CLIENT_ID));
-            assertTrue(authUrl.getProtocol().equals("https"));
-        } catch (MalformedURLException e) {
-            Assertions.fail();
-        }
-    }
+	@Test
+	void createAuthUrl_success() throws DuoException {
+		String urlString = client.createAuthUrl(USERNAME, STATE, HTTPS_REDIRECT_URI);
+		try {
+			URL authUrl = new URL(urlString);
+			assertEquals(authUrl.getHost(), API_HOST);
+			assertTrue(authUrl.getQuery().contains("redirect_uri=" + HTTPS_REDIRECT_URI));
+			assertTrue(authUrl.getQuery().contains("client_id=" + CLIENT_ID));
+			assertTrue(authUrl.getProtocol().equals("https"));
+		} catch (MalformedURLException e) {
+			Assertions.fail();
+		}
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_username() {
-        try {
-            client.createAuthUrl("", STATE);
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().equals("Missing username"));
-        }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_username() {
+		try {
+			client.createAuthUrl("", STATE, HTTPS_REDIRECT_URI);
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().equals("Missing username"));
+		}
 
-    }
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_state() {
-        try {
-            client.createAuthUrl(USERNAME, "");
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().equals("Invalid state"));
-        }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_state() {
+		try {
+			client.createAuthUrl(USERNAME, "", HTTPS_REDIRECT_URI);
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().equals("Invalid state"));
+		}
 
-    }
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_client_id() throws DuoException {
-        try {
-            Client badClient = new Client.Builder("", CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
-            badClient.createAuthUrl(USERNAME, STATE);
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().equals("Invalid client id"));
-        }
-    }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_client_id() throws DuoException {
+		try {
+			Client badClient = new Client.Builder("", CLIENT_SECRET, API_HOST).build();
+			badClient.createAuthUrl(USERNAME, STATE, HTTPS_REDIRECT_URI);
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().equals("Invalid client id"));
+		}
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_client_secret() throws DuoException {
-        try {
-            Client badClient = new Client.Builder(CLIENT_ID, "", API_HOST, HTTPS_REDIRECT_URI).build();
-            badClient.createAuthUrl(USERNAME, STATE);
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().equals("Invalid client secret"));
-        }
-    }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_client_secret() throws DuoException {
+		try {
+			Client badClient = new Client.Builder(CLIENT_ID, "", API_HOST).build();
+			badClient.createAuthUrl(USERNAME, STATE, HTTPS_REDIRECT_URI);
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().equals("Invalid client secret"));
+		}
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_api_host() throws DuoException {
-        try {
-            Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, "", HTTPS_REDIRECT_URI).build();
-            badClient.createAuthUrl(USERNAME, STATE);
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().contains("Invalid host"));
-        }
-    }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_api_host() throws DuoException {
+		try {
+			Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, "").build();
+			badClient.createAuthUrl(USERNAME, STATE, "");
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().contains("Invalid host"));
+		}
+	}
 
-    @Test
-    void createAuthUrl_throws_exception_for_invalid_redirect_uri() throws DuoException {
-        try {
-            Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, "").build();
-            badClient.createAuthUrl(USERNAME, STATE);
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().contains("no protocol: "));
-        }
-    }
+	@Test
+	void createAuthUrl_throws_exception_for_invalid_redirect_uri() throws DuoException {
+		try {
+			Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST).build();
+			badClient.createAuthUrl(USERNAME, STATE, "");
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().contains("no protocol: "));
+		}
+	}
 
-    @Test
-    void exchangeAuthorizationCodeFor2FAResult_success() throws DuoException {
-        TokenResponse tokenResponse = new TokenResponse();
-        tokenResponse.setId_token("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.qKLZNpctaGsuqLr6KkPiM7_9jG5sEEaLPLakrA1kjk7z0lF3HX_RTRS3c4wVFWMEV_jGg72KIjlBpsWrqMxSNg");
-        Mockito.when(client.duoConnector.exchangeAuthorizationCodeFor2FAResult(
-                anyString(), anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn(tokenResponse);
+	@Test
+	void exchangeAuthorizationCodeFor2FAResult_success() throws DuoException {
+		TokenResponse tokenResponse = new TokenResponse();
+		tokenResponse.setId_token(
+				"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.qKLZNpctaGsuqLr6KkPiM7_9jG5sEEaLPLakrA1kjk7z0lF3HX_RTRS3c4wVFWMEV_jGg72KIjlBpsWrqMxSNg");
+		Mockito.when(client.duoConnector.exchangeAuthorizationCodeFor2FAResult(anyString(), anyString(), anyString(),
+				anyString(), anyString(), anyString())).thenReturn(tokenResponse);
 
-        TokenValidator stubValidator = new TokenValidator() {
-            @Override
-            public DecodedJWT validateAndDecode(String jwt) throws DuoException {
-                return JWT.decode(jwt);
-            }
-        };
+		TokenValidator stubValidator = new TokenValidator() {
+			@Override
+			public DecodedJWT validateAndDecode(String jwt) throws DuoException {
+				return JWT.decode(jwt);
+			}
+		};
 
-        Token result = client.exchangeAuthorizationCodeFor2FAResult("duo_code", stubValidator);
-        assertEquals(result.getSub(), "1234567890");
-    }
+		Token result = client.exchangeAuthorizationCodeFor2FAResult("duo_code", stubValidator, HTTPS_REDIRECT_URI);
+		assertEquals(result.getSub(), "1234567890");
+	}
 
-    @Test
-    void exchangeAuthorizationCodeFor2FAResult_throws_exception_for_invalid_api_host() throws DuoException {
-        try {
-            Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, "", HTTPS_REDIRECT_URI).build();
-            badClient.exchangeAuthorizationCodeFor2FAResult("duo_code", "username");
-            Assertions.fail();
-        } catch (DuoException e) {
-            assertTrue(e.getMessage().contains("Invalid host"));
-        }
-    }
+	@Test
+	void exchangeAuthorizationCodeFor2FAResult_throws_exception_for_invalid_api_host() throws DuoException {
+		try {
+			Client badClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, "").build();
+			badClient.exchangeAuthorizationCodeFor2FAResult("duo_code", "username", HTTPS_REDIRECT_URI);
+			Assertions.fail();
+		} catch (DuoException e) {
+			assertTrue(e.getMessage().contains("Invalid host"));
+		}
+	}
 
-    @Test
-    void useDuoCodeAttribute_defaults_true() throws DuoException {
-        // Don't rely on setUp Client just in case that ever changes to longform constructor
-        Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
-        String urlString = client.createAuthUrl(USERNAME, STATE);
-        HttpUrl url = HttpUrl.parse(urlString);
-        String jwtString = url.queryParameter("request");
-        DecodedJWT jwt = JWT.decode(jwtString);
-        Boolean use_code = jwt.getClaim("use_duo_code_attribute").asBoolean();
-        assertTrue(use_code);
-    }
+	@Test
+	void useDuoCodeAttribute_defaults_true() throws DuoException {
+		// Don't rely on setUp Client just in case that ever changes to longform
+		// constructor
+		Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST).build();
+		String urlString = client.createAuthUrl(USERNAME, STATE, HTTPS_REDIRECT_URI);
+		HttpUrl url = HttpUrl.parse(urlString);
+		String jwtString = url.queryParameter("request");
+		DecodedJWT jwt = JWT.decode(jwtString);
+		Boolean use_code = jwt.getClaim("use_duo_code_attribute").asBoolean();
+		assertTrue(use_code);
+	}
 
-    @Test
-    void useDuoCodeAttribute_set_false() throws DuoException {
-        Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI)
-                .setUseDuoCodeAttribute(false)
-                .build();
-        String urlString = client.createAuthUrl(USERNAME, STATE);
-        HttpUrl url = HttpUrl.parse(urlString);
-        String jwtString = url.queryParameter("request");
-        DecodedJWT jwt = JWT.decode(jwtString);
-        Boolean use_code = jwt.getClaim("use_duo_code_attribute").asBoolean();
-        assertFalse(use_code);
-    }
+	@Test
+	void useDuoCodeAttribute_set_false() throws DuoException {
+		Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST).setUseDuoCodeAttribute(false).build();
+		String urlString = client.createAuthUrl(USERNAME, STATE, HTTPS_REDIRECT_URI);
+		HttpUrl url = HttpUrl.parse(urlString);
+		String jwtString = url.queryParameter("request");
+		DecodedJWT jwt = JWT.decode(jwtString);
+		Boolean use_code = jwt.getClaim("use_duo_code_attribute").asBoolean();
+		assertFalse(use_code);
+	}
 
-    @Test
-    void custom_useragent() throws DuoException {
-        String appendedUserAgent = "foobar";
+	@Test
+	void custom_useragent() throws DuoException {
+		String appendedUserAgent = "foobar";
 
-        Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI)
-                .appendUserAgentInfo(appendedUserAgent)
-                .build();
+		Client client = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST).appendUserAgentInfo(appendedUserAgent)
+				.build();
 
-        client.duoConnector = Mockito.mock(DuoConnector.class);
+		client.duoConnector = Mockito.mock(DuoConnector.class);
 
-        try {
-            client.exchangeAuthorizationCodeFor2FAResult("duo_code", Mockito.mock(TokenValidator.class));
-        }
-        catch(Exception e) {
-            // The calls will fail due to the incomplete mocking, but we're only interesting
-            // in the calling arguments, so this is fine and can be ignored.
-        }
+		try {
+			client.exchangeAuthorizationCodeFor2FAResult("duo_code", Mockito.mock(TokenValidator.class),
+					HTTPS_REDIRECT_URI);
+		} catch (Exception e) {
+			// The calls will fail due to the incomplete mocking, but we're only interesting
+			// in the calling arguments, so this is fine and can be ignored.
+		}
 
-        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
-        verify(client.duoConnector).exchangeAuthorizationCodeFor2FAResult(stringCaptor.capture(), anyString(), anyString(), anyString(), anyString(), anyString());
-        String sentUserAgent = stringCaptor.getValue();
-        assertTrue(sentUserAgent.startsWith("duo_universal_java") && sentUserAgent.endsWith(appendedUserAgent));
-    }
+		ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+		verify(client.duoConnector).exchangeAuthorizationCodeFor2FAResult(stringCaptor.capture(), anyString(),
+				anyString(), anyString(), anyString(), anyString());
+		String sentUserAgent = stringCaptor.getValue();
+		assertTrue(sentUserAgent.startsWith("duo_universal_java") && sentUserAgent.endsWith(appendedUserAgent));
+	}
 
-    @Test
-    void legacy_constructors_match() throws DuoException {
-        // Create clients using the old deprecated constructors and check that their fields are the same as one created using the builder.
-        // This should help prevent adding a new field to the class and forgetting to update the legacy constructors.
-        // Unfortunately duoConnector is an object entity that can't be compared.
+	@Test
+	void legacy_constructors_match() throws DuoException {
+		// Create clients using the old deprecated constructors and check that their
+		// fields are the same as one created using the builder.
+		// This should help prevent adding a new field to the class and forgetting to
+		// update the legacy constructors.
+		// Unfortunately duoConnector is an object entity that can't be compared.
 
-        Client builderClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
-        Client shortConstructorClient = new Client(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI);
-        Client longConstructorClient = new Client(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI, null);
+		Client builderClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST).build();
+		Client shortConstructorClient = new Client(CLIENT_ID, CLIENT_SECRET, API_HOST);
+		Client longConstructorClient = new Client(CLIENT_ID, CLIENT_SECRET, API_HOST, null);
 
-        assertTrue(new ReflectionEquals(builderClient, "duoConnector").matches(shortConstructorClient));
-        assertTrue(new ReflectionEquals(builderClient, "duoConnector").matches(longConstructorClient));
-    }
+		assertTrue(new ReflectionEquals(builderClient, "duoConnector").matches(shortConstructorClient));
+		assertTrue(new ReflectionEquals(builderClient, "duoConnector").matches(longConstructorClient));
+	}
 
 }
